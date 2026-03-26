1fd8fd727c02faf4b8dcc6ecb4ff7aee94e316ab
DELL <DELL@DESKTOP-2EK0VDO>
2026-03-26 00:01:29 -0700

    Add project files.

---
+++ InteractivePasswordField/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace InteractivePasswordField
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Interactive Password Field";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(35, 30);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(624, 26);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.DoubleClick += new System.EventHandler(this.txtPassword_DoubleClick);
+            this.txtPassword.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtPassword_MouseDown);
+            this.txtPassword.MouseEnter += new System.EventHandler(this.txtPassword_MouseEnter);
+            this.txtPassword.MouseLeave += new System.EventHandler(this.txtPassword_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Location = new System.Drawing.Point(53, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 198);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(499, 145);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Interactive Features:\r\n• Hover - Reveal password\r\n• Right-click - Generate random" +
+    " password\r\n• Left-click - Copy to clipboard\r\n• Double-click - Clear password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblStatus.Location = new System.Drawing.Point(285, 534);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(213, 32);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status:  Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 639);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ InteractivePasswordField/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InteractivePasswordField
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        // 🔄 Double Click → Clear
+        private void txtPassword_DoubleClick(object sender, EventArgs e)
+        {
+            txtPassword.Clear();
+            txtPassword.UseSystemPasswordChar = true;
+            lblStatus.Text = "Status: Password Cleared";
+        }
+
+        // 👁️ Hover → Show Password
+        private void txtPassword_MouseEnter(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = false;
+        }
+
+        // 🔒 Leave → Hide Password
+        private void txtPassword_MouseLeave(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        // 🖱️ Mouse Down → Left/Right Click Actions
+        private void txtPassword_MouseDown(object sender, MouseEventArgs e)
+        {
+            // 👉 RIGHT CLICK → Generate password
+            if (e.Button == MouseButtons.Right)
+            {
+                txtPassword.Text = GeneratePassword(10);
+                txtPassword.UseSystemPasswordChar = true;
+                lblStatus.Text = "Status: Random Password Generated";
+            }
+
+            // 👉 LEFT CLICK → Copy password
+            else if (e.Button == MouseButtons.Left)
+            {
+                if (!string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    Clipboard.SetText(txtPassword.Text);
+                    lblStatus.Text = "Status: Password Copied";
+                }
+            }
+        }
+
+        // 🔐 Password Generator
+        private string GeneratePassword(int length)
+        {
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            string symbols = "!@#$%^&*";
+
+            string all = lower + upper + numbers + symbols;
+
+            StringBuilder password = new StringBuilder();
+
+            password.Append(lower[random.Next(lower.Length)]);
+            password.Append(upper[random.Next(upper.Length)]);
+            password.Append(numbers[random.Next(numbers.Length)]);
+            password.Append(symbols[random.Next(symbols.Length)]);
+
+            for (int i = 4; i < length; i++)
+            {
+                password.Append(all[random.Next(all.Length)]);
+            }
+
+            return new string(password.ToString().OrderBy(x => random.Next()).ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
